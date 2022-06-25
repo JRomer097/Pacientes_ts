b9886e1a8d41c60964ffb379899950d4208acc74
Jose Romero <josecruz8383@gmail.com>
2022-06-24 22:43:41 -0500

    Prueba desde casa

---
+++ app/Http/Controllers/PacientesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\prueba_1;
+use Illuminate\Http\Request;
+
+class PacientesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\prueba_1  $prueba_1
+     * @return \Illuminate\Http\Response
+     */
+    public function show(prueba_1 $prueba_1)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\prueba_1  $prueba_1
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(prueba_1 $prueba_1)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\prueba_1  $prueba_1
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, prueba_1 $prueba_1)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\prueba_1  $prueba_1
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(prueba_1 $prueba_1)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pacientes;
+class PruebaController extends Controller
+{
+    //Mandamos y mostramos la información de la DB
+    public function index()
+    {
+        $pacientes = Pacientes::all();
+        return view('add_pacientes', [
+            'pacientes' => $pacientes
+        ]);
+    }
+
+    //Ventana para graficar la informacion del paciente
+    public function graficar(Pacientes $pacientes)
+    {
+        $datos = $pacientes;
+        return view('grafica', ['datos' => $datos]);
+        //return $datos;
+    }
+
+    //Ventana para editar los datos del paciente
+    public function editar($pacientes)
+    {
+        $datos = Pacientes::find($pacientes);
+        return view('Editar', ['datos' => $datos]);
+    }
+
+    //Actualizar la informacion del paciente
+    public function update(Request $request, $pacientes)
+    {
+        $paciente = Pacientes::find($pacientes);
+        $paciente->nombre_p = $request ->nombre_p;
+        $paciente->apellidoP_p = $request ->apellidoP_p;
+        $paciente->apellidoM_p = $request ->apellidoM_p;
+        $paciente->edad = $request ->edad;
+        $paciente->peso = $request ->peso;
+        $paciente->altura = $request ->altura;
+        $paciente->tipo_de_sangre = $request ->tipo_de_sangre;
+        $paciente -> save();
+        return redirect() -> route('pacientes.index');
+    }
+
+    //Guarda la informacion del nuevo paciente
+    public function store(Request $request)
+    {
+        //Validamos los parametros
+        $validar = $request -> validate([
+            'nombre_p' => 'required',
+            'apellidoP_p' => 'required',
+            'apellidoM_p' => 'required',
+            'edad' => 'required|numeric|min:3',
+            'peso' =>'required',
+            'altura' => 'required',
+            'tipo_de_sangre' => 'required|min:2'
+            
+        ]);
+
+        //Guardamos la informción del nuevo paciente al validarlo
+        Pacientes::create([
+            'nombre_p' => $request -> nombre_p,
+            'apellidoP_p' => $request -> apellidoP_p,
+            'apellidoM_p' => $request -> apellidoM_p,
+            'edad' => $request -> edad,
+            'peso' => $request -> peso,
+            'altura' => $request -> altura,
+            'tipo_de_sangre' => $request -> tipo_de_sangre
+        ]);
+
+        return back();
+    }
+
+    //Borra la informacion del paciente
+    public function delete(Pacientes $pacientes)
+    {
+        $pacientes ->delete();
+        return back();
+    }
+
+}
